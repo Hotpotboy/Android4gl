4526296465889974e9b6c80ac677dbd471c199eb
zhanghang <407376249@qq.com>
2019-11-05 00:51:36 +0800

    调通立方体渲染
    Signed-off-by: zhanghang <407376249@qq.com>

--- app/src/main/java/com/zhanghang/android4gl/opengl/ClubDrawer.java
+++ app/src/main/java/com/zhanghang/android4gl/opengl/ClubDrawer.java
@@ -14,109 +14,58 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class ClubDrawer extends BaseDrawer {
-    private static final float UNIT = 10f;
+    private static final float UNIT = 1f;
 
-    private static final float[] CUBE_TRIANGLES_VERTEXS  =
-            {
-                    // Front face
-                    -1.0f, 1.0f, 1.0f,
-                    -1.0f, -1.0f, 1.0f,
-                    1.0f, 1.0f, 1.0f,
-                    -1.0f, -1.0f, 1.0f,
-                    1.0f, -1.0f, 1.0f,
-                    1.0f, 1.0f, 1.0f,
+    private static final float[] CUBE_TRIANGLES_VERTEXS = {
+            0, 0, 0,//V1
+            0, UNIT, 0,//v4
+            UNIT, 0, 0, // v2
 
-                    // Right face
-                    1.0f, 1.0f, 1.0f,
-                    1.0f, -1.0f, 1.0f,
-                    1.0f, 1.0f, -1.0f,
-                    1.0f, -1.0f, 1.0f,
-                    1.0f, -1.0f, -1.0f,
-                    1.0f, 1.0f, -1.0f,
+            UNIT, 0, 0, // v2
+            0, UNIT, 0,//v4
+            UNIT, UNIT, 0,//V3
 
-                    // Back face
-                    1.0f, 1.0f, -1.0f,
-                    1.0f, -1.0f, -1.0f,
-                    -1.0f, 1.0f, -1.0f,
-                    1.0f, -1.0f, -1.0f,
-                    -1.0f, -1.0f, -1.0f,
-                    -1.0f, 1.0f, -1.0f,
+            UNIT, 0, 0, // v2
+            UNIT, UNIT, 0,//V3
+            UNIT, UNIT, UNIT,//V7
 
-                    // Left face
-                    -1.0f, 1.0f, -1.0f,
-                    -1.0f, -1.0f, -1.0f,
-                    -1.0f, 1.0f, 1.0f,
-                    -1.0f, -1.0f, -1.0f,
-                    -1.0f, -1.0f, 1.0f,
-                    -1.0f, 1.0f, 1.0f,
+            UNIT, 0, 0, // v2
+            UNIT, UNIT, UNIT,//V7
+            UNIT, 0, UNIT,//V6
 
-                    // Top face
-                    -1.0f, 1.0f, -1.0f,
-                    -1.0f, 1.0f, 1.0f,
-                    1.0f, 1.0f, -1.0f,
-                    -1.0f, 1.0f, 1.0f,
-                    1.0f, 1.0f, 1.0f,
-                    1.0f, 1.0f, -1.0f,
+            UNIT, UNIT, UNIT,//V7
+            UNIT, 0, UNIT,//V6
+            0, UNIT, UNIT, // v8
 
-                    // Bottom face
-                    1.0f, -1.0f, -1.0f,
-                    1.0f, -1.0f, 1.0f,
-                    -1.0f, -1.0f, -1.0f,
-                    1.0f, -1.0f, 1.0f,
-                    -1.0f, -1.0f, 1.0f,
-                    -1.0f, -1.0f, -1.0f,
-            };
+            UNIT, 0, UNIT,//V6
+            0, UNIT, UNIT, // v8
+            0, 0, UNIT, // v5
 
-//    private static final float[] CUBE_TRIANGLES_VERTEXS = {
-//            0, 0, 0,//V1
-//            0, UNIT, 0,//v4
-//            UNIT, 0, 0, // v2
-//
-//            UNIT, 0, 0, // v2
-//            0, UNIT, 0,//v4
-//            UNIT, UNIT, 0,//V3
-//
-//            UNIT, 0, 0, // v2
-//            UNIT, UNIT, 0,//V3
-//            UNIT, UNIT, UNIT,//V7
-//
-//            UNIT, 0, 0, // v2
-//            UNIT, UNIT, UNIT,//V7
-//            UNIT, 0, UNIT,//V6
-//
-//            UNIT, UNIT, UNIT,//V7
-//            UNIT, 0, UNIT,//V6
-//            0, UNIT, UNIT, // v8
-//
-//            UNIT, 0, UNIT,//V6
-//            0, UNIT, UNIT, // v8
-//            0, 0, UNIT, // v5
-//
-//            0, UNIT, UNIT, // v8
-//            0, 0, UNIT, // v5
-//            0, 0, 0,//V1
-//
-//            0, 0, 0,//V1
-//            0, UNIT, 0,//v4
-//            0, UNIT, UNIT, // v8
-//
-//            0, UNIT, 0,//v4
-//            0, UNIT, UNIT, // v8
-//            UNIT, UNIT, UNIT,//V7
-//
-//            0, UNIT, 0,//v4
-//            UNIT, UNIT, UNIT,//V7
-//            UNIT, UNIT, 0,//V3
-//
-//            0, 0, 0,//V1
-//            0, 0, UNIT, // v5
-//            UNIT, 0, UNIT,//V6
-//
-//            0, 0, 0,//V1
-//            UNIT, 0, UNIT,//V6
-//            UNIT, 0, 0, // v2
-//    };
-    private static final String MVPMATRIX_NAME = "u_mvpMatrix";
+            0, UNIT, UNIT, // v8
+            0, 0, UNIT, // v5
+            0, 0, 0,//V1
+
+            0, 0, 0,//V1
+            0, UNIT, 0,//v4
+            0, UNIT, UNIT, // v8
+
+            0, UNIT, 0,//v4
+            0, UNIT, UNIT, // v8
+            UNIT, UNIT, UNIT,//V7
+
+            0, UNIT, 0,//v4
+            UNIT, UNIT, UNIT,//V7
+            UNIT, UNIT, 0,//V3
+
+            0, 0, 0,//V1
+            0, 0, UNIT, // v5
+            UNIT, 0, UNIT,//V6
+
+            0, 0, 0,//V1
+            UNIT, 0, UNIT,//V6
+            UNIT, 0, 0, // v2
+    };
+    private static final String MVPMATRIX_NAME = "mvpMatrix";
 
     //立方体的顶点颜色
     private float[] COLORS = {
@@ -185,9 +134,8 @@ public class ClubDrawer extends BaseDrawer {
     private float[] mProjectMatrix = new float[16];
 
     private float[] mViewMatrix = new float[16];
-    private FloatBuffer mMVPMatrixBuffer;
+
     private float[] mMVPMatrix;
-    private float[] mModelMatrix = new float[16];
 
     public ClubDrawer(Context context) {
         super(context, R.raw.cube_vertex_shader, R.raw.cube_fragment_shader);
@@ -223,51 +171,32 @@ public class ClubDrawer extends BaseDrawer {
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
         float ratio = (float) width / height;
-        Matrix.frustumM(mProjectMatrix, 0, -ratio, ratio, -1, 1, 0.1f, 10);
+        Matrix.frustumM(mProjectMatrix, 0, -ratio, ratio, -1, 1, 3f, 20);
     }
+
+    private float mRotationAngle = -10;
+
+    private long mCurrentTime;
 
     @Override
     public void onDrawFrame(GL10 gl) {
-        GLES20.glClearColor(1, 1, 1, 1);
+        GLES20.glClearColor(1, 1, 0, 1);
 
-
-        final float eyeX = 0.0f;
-        final float eyeY = 0.0f;
-        final float eyeZ = -0.5f;
-
-        // We are looking toward the distance
-        final float lookX = 0.0f;
-        final float lookY = 0.0f;
-        final float lookZ = -5.0f;
-
-        // Set our up vector. This is where our head would be pointing were we holding the camera.
-        final float upX = 0.0f;
-        final float upY = 1.0f;
-        final float upZ = 0.0f;
-
-        // Set the view matrix. This matrix can be said to represent the camera position.
-        // NOTE: In OpenGL 1, a ModelView matrix is used, which is a combination of a model and
-        // view matrix. In OpenGL 2, we can keep track of these matrices separately if we choose.
-        Matrix.setLookAtM(mViewMatrix, 0, eyeX, eyeY, eyeZ, lookX, lookY, lookZ, upX, upY, upZ);
-
-
-        Matrix.setIdentityM(mModelMatrix, 0);
-        Matrix.translateM(mModelMatrix, 0, 0.0f, 0.0f, -5.0f);
-        Matrix.rotateM(mModelMatrix, 0, 90, 1.0f, 1.0f, 0.0f);
+        Matrix.setIdentityM(mViewMatrix, 0);
+        if (System.currentTimeMillis() - mCurrentTime >= 500) {
+            mRotationAngle += 10;
+            mCurrentTime = System.currentTimeMillis();
+        }
+        Matrix.rotateM(mViewMatrix, 0, mRotationAngle, 0, 1, 0);
+        Matrix.translateM(mViewMatrix, 0, 0, 0, 10f);
+        float[] tmp = new float[16];
+        System.arraycopy(mViewMatrix, 0, tmp, 0, 16);
+        Matrix.invertM(mViewMatrix, 0, tmp, 0);
 
         if (mMVPMatrix == null) {
             mMVPMatrix = new float[16];
         }
-
-        Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mModelMatrix, 0);
-        Matrix.multiplyMM(mMVPMatrix, 0, mProjectMatrix, 0, mMVPMatrix, 0);
-
-        if (mMVPMatrixBuffer == null) {
-            mMVPMatrixBuffer = ByteBuffer.allocateDirect(16 * 4).asFloatBuffer();
-        }
-        mMVPMatrixBuffer.position(0);
-        mMVPMatrixBuffer.put(mMVPMatrix);
-        mMVPMatrixBuffer.position(0);
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectMatrix, 0, mViewMatrix, 0);
 
         GLES20.glUseProgram(mProgram);
 
@@ -275,7 +204,7 @@ public class ClubDrawer extends BaseDrawer {
         GLES20.glVertexAttribPointer(mPositionHandler, 3, GLES20.GL_FLOAT, false, 0, mCubeTrianglesVertexsBuffer);
         GLES20.glEnableVertexAttribArray(mColorHandler);
         GLES20.glVertexAttribPointer(mColorHandler, 4, GLES20.GL_FLOAT, false, 0, mColorBuffer);
-        GLES20.glUniformMatrix4fv(mMVPMatrixHandler, 1, false, mMVPMatrixBuffer);
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandler, 1, false, mMVPMatrix, 0);
 
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, CUBE_TRIANGLES_VERTEXS.length / 3);
 
